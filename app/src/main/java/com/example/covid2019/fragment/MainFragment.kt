8e5b9b70a49f04bb01d795ff98991f39220e1f16
Huy Cat Hoang <cat.hoanghuy@gmail.com>
2020-04-10 14:29:33 +1000

    updated - added main fragment which show conversation with bot

--- app/src/main/java/com/example/covid2019/fragment/MainFragment.kt
+++ app/src/main/java/com/example/covid2019/fragment/MainFragment.kt
@@ -5,13 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.example.covid2019.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.covid2019.databinding.FragmentMainBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -23,11 +20,11 @@ class MainFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var binding: FragmentMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -35,8 +32,10 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        val v = binding.root
+
+        return v
     }
 
     companion object {
@@ -53,8 +52,7 @@ class MainFragment : Fragment() {
         fun newInstance(param1: String, param2: String) =
             MainFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
     }
