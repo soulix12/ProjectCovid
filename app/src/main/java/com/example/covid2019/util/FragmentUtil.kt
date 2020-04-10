342764cbdcc3a5c25a945a992c3584390dc3497c
Huy Cat Hoang <cat.hoanghuy@gmail.com>
2020-04-10 14:48:54 +1000

    updated - added function to setup drawer

--- app/src/main/java/com/example/covid2019/util/FragmentUtil.kt
+++ app/src/main/java/com/example/covid2019/util/FragmentUtil.kt
@@ -1,2 +1,12 @@
 package com.example.covid2019.util
 
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+
+fun Fragment.initToolbar(toolbar: Toolbar, titleResId: Int, backEnabled: Boolean) {
+    val appCompatActivity = activity as AppCompatActivity
+    appCompatActivity.setSupportActionBar(toolbar)
+    appCompatActivity.supportActionBar?.setTitle(titleResId)
+    appCompatActivity.supportActionBar?.setDisplayHomeAsUpEnabled(backEnabled)
+}
