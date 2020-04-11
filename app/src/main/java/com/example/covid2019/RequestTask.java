91d0cd082bb9a0d2b10a323e385cdd30661ea038
Huy Cat Hoang <cat.hoanghuy@gmail.com>
2020-04-11 12:26:11 +1000

    updated - added message handler

--- app/src/main/java/com/example/covid2019/RequestTask.java
+++ app/src/main/java/com/example/covid2019/RequestTask.java
@@ -1,7 +1,11 @@
-package com.tyagiabhinav.dialogflowchat;
+package com.example.covid2019;
 
 import android.app.Activity;
 import android.os.AsyncTask;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.covid2019.fragment.HomeFragment;
 
 import ai.api.AIServiceContext;
 import ai.api.AIServiceException;
@@ -9,13 +13,13 @@ import ai.api.android.AIDataService;
 import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
 
-public class RequestTask  extends AsyncTask<AIRequest, Void, AIResponse> {
+public class RequestTask extends AsyncTask<AIRequest, Void, AIResponse> {
 
-    Activity activity;
+    Fragment activity;
     private AIDataService aiDataService;
     private AIServiceContext customAIServiceContext;
 
-    RequestTask(Activity activity, AIDataService aiDataService, AIServiceContext customAIServiceContext){
+    public RequestTask(Fragment activity, AIDataService aiDataService, AIServiceContext customAIServiceContext){
         this.activity = activity;
         this.aiDataService = aiDataService;
         this.customAIServiceContext = customAIServiceContext;
@@ -34,6 +38,6 @@ public class RequestTask  extends AsyncTask<AIRequest, Void, AIResponse> {
 
     @Override
     protected void onPostExecute(AIResponse aiResponse) {
-        ((MainActivity)activity).callback(aiResponse);
+        ((HomeFragment)activity).callback(aiResponse);
     }
 }
